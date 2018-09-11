99afc978d07d74fec3f890c5aa49c4eee76ba2d7
kingfishar <ek.chibueze@gmail.com>
2018-09-11 13:45:46 +0300

    Added a simple addition program.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,11 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addBtn = (Button) findViewById(R.id.addBtn); //object of button type
+        addBtn.setOnClickListener(new View.OnClickListener() { //sets OnClickListener
+            @Override
+            public void onClick(View view) {
+                EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText); //object of EditText type
+                EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText); //object of EditText type
+                TextView resTextView = (TextView) findViewById(R.id.resTextView); //object of TextView type
+
+                int num1 = Integer.parseInt(firstNumEditText.getText().toString()); //gets the firstEditText value
+                int num2 = Integer.parseInt(secondNumEditText.getText().toString()); //gets the secondEditText value
+                int result = num1 + num2; //adds the two values
+                resTextView.setText(result + ""); //displays the reults
+
+            }
+        });
     }
-    //no comment yet
-    //for(i = 0; i <= 10; i++){
-        //do something here!
-    //}
-    //comment from github
-    //another commit
 }
